d0eaaa3f311a906e53b7a5530dd95281a0709f12
Hiro102520 <hirofumi.kobean102520@gmail.com>
2021-12-15 22:38:40 +0900

    log:チェックボックスをクリックした時に人口を取得できる機能のコーディング途中

--- src/Components/PrefCheckBox.jsx
+++ src/Components/PrefCheckBox.jsx
@@ -17,11 +17,40 @@ export const PrefCheckBox = () => {
       });
   }, []);
   // console.log(prefectures);
+
+  const [prefPopulation, setPrefPopulation] = useState([]);
+  //チェックボックスをクリックした時の処理
+  const clickCheckbox = (prefCode, check) => {
+    if (check) {
+      axios
+        .get(
+          'https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=' +
+            String(prefCode),
+          {
+            headers: { 'X-API-KEY': process.env.REACT_APP_API_KEY },
+          },
+        )
+        .then((results) => {
+          setPrefPopulation(results);
+          console.log(prefPopulation);
+        })
+        .catch((error) => {
+          console.log(error);
+          return;
+        });
+    }
+  };
+
   return (
     <>
       {prefectures.map((prefecture) => (
         <li key={prefecture.prefName}>
-          <input type="checkbox" name="pref_name" />
+          <input
+            type="checkbox"
+            name="pref_name"
+            onClick={clickCheckbox}
+            onChange={clickCheckbox}
+          />
           <label>{prefecture.prefName}</label>
         </li>
       ))}
